ecb37b6c1c87daee786742288e0edd32ce2e78d2
shamimzn <shamim.zohourian@gmail.com>
2021-01-18 17:32:46 -0500

    aa

---
+++ FinalTestGeneric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalTestGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
