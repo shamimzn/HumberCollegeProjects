f6f52b995a3c7bfcfdb976376645fc4bfc3d551c
shamimzn <shamim.zohourian@gmail.com>
2021-01-18 19:57:54 -0500

    Some Changes

--- FinalTestEvents/Program.cs
+++ FinalTestEvents/Program.cs
@@ -6,7 +6,53 @@ namespace FinalTestEvents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Alarm ar = new Alarm();
+            Fier fr = new Fier();
+            ar.SomeEvent += fr.WakeUP;
+            ar.FireAlarm(DateTime.Now);
+        }
+    }
+
+
+
+    public class Alarm {
+
+        public event MyDelegate SomeEvent;
+        public void FireAlarm(DateTime DT)
+        {
+            if (SomeEvent != null)
+            {
+                SomeEvent(this, new DelegateEvent(DT));
+
+
+            }
+
+        }
+    
+    }
+
+
+    public class Fier
+    {
+
+        public void WakeUP(object sender, DelegateEvent e)
+        {
+            Console.WriteLine("wakeup {0}", e.SetDate.ToString());
+
+        }
+    }
+
+
+    public delegate void MyDelegate(object sender, DelegateEvent e);
+
+    public class DelegateEvent : EventArgs
+    { 
+    
+    public DateTime SetDate { get; set; }
+        public DelegateEvent(DateTime setTime)
+        {
+
+            SetDate = setTime;
         }
     }
 }
