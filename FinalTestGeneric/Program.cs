8be780b2a8c8c3233bf78bfa139f091fe8082f02
shamimzn <shamim.zohourian@gmail.com>
2021-01-18 21:14:09 -0500

    changed

--- FinalTestGeneric/Program.cs
+++ FinalTestGeneric/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FinalTestGeneric
 {
@@ -6,7 +7,47 @@ namespace FinalTestGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+          
+
+            int[] cars = { 4, 6, 1, 9 };
+                          
+
+            MyGenericClass<int>.AddItem(cars);
+
+            Console.WriteLine("Get Item 2 :  {0}",MyGenericClass<int>.GetSingleItem(2).ToString());
+
+            foreach (int str in MyGenericClass<int>.GetAllSortedDesc())
+             Console.WriteLine(str.ToString());
+
+        }
+    }
+
+    public  static class MyGenericClass<T>
+    {
+        
+
+        private static  List<T> myList;
+
+        public static void AddItem(T[] items)
+        {
+            myList = new List<T>();
+            foreach (T item in items)
+            {
+                myList.Add(item);
+            }
+        }
+
+        public static T GetSingleItem(int Index)
+        {
+            return myList[Index];
+        }
+
+        public static List<T> GetAllSortedDesc()
+        {
+            myList.Sort(); //it sortes acending
+            myList.Reverse(); // it sorts Descending
+            return myList;
         }
     }
 }
